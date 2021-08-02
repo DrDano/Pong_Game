02b11661bd0cf904f98194c2dbb3209971c13a6b
DrDano <danielharned@gmail.com>
2021-08-02 16:58:44 -0700

    Finished creating go_up and go_down functions (day 22 100DoC) 08-02-21

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ screen = Screen()
 screen.setup(height=600, width=800)
 screen.title(titlestring="Pong")
 screen.bgcolor("black")
+screen.tracer(0)
 
-paddle.Paddle()
+user_paddle = paddle.Paddle()
 
 screen.listen()
-screen.onkey(fun=paddle.Paddle.go_up, key="Up")
+screen.onkey(fun=user_paddle.go_up(), key="Up")
+screen.onkey(fun=user_paddle.go_down(), key="Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
